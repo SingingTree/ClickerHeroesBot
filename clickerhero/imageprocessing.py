f46b8ddc62e342a66dc364b209e592d2cd55774e
Bryce Van Dyk <bryce@vandyk.net.nz>
2015-06-12 19:47:16 +1200

    Restore OpenCV template match functionality test
    OpenCV template match appears to be working after bumping from RC1 to
    final release. Not sure if user error or if bugs have been fixed, but am
    happy with the result.

--- clickerhero/imageprocessing.py
+++ clickerhero/imageprocessing.py
@@ -17,61 +17,63 @@ def pixel_differences(img, template):
     return difference
 
 
-img = cv2.imread('img/FishOnScreen.png', cv2.IMREAD_UNCHANGED)
+#img = cv2.imread('img/FishOnScreen.png', cv2.IMREAD_UNCHANGED)
+img = cv2.imread('img/FishOnScreen.png')
 
 img2 = img.copy()
-template = cv2.imread('img/Fish.png', cv2.IMREAD_UNCHANGED)
+# template = cv2.imread('img/Fish.png', cv2.IMREAD_UNCHANGED)
+template = cv2.imread('img/Fish.png')
 
 img_width, img_height, img_depth = img.shape
 template_width, template_height, template_depth  = template.shape
-
-min_pixel_difference = pixel_differences(img, template)
-min_difference_top_left = (0, 0)
-row = 0
-col = 1
-while(row + template_height < img_height):
-    while(col + template_width < img_width):
-        pixel_difference = pixel_differences(img[row:, col:], template)
-        if pixel_difference < min_pixel_difference:
-            min_pixel_difference = pixel_difference
-            min_difference_top_left = (col, row)
-        col += 1
-        print("process col")
-    row += 1
-
-
-img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGBA)
-template= cv2.cvtColor(template, cv2.COLOR_BGRA2RGBA)
-
-plt.imshow(img)
-plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
-plt.show()
+#
+# min_pixel_difference = pixel_differences(img, template)
+# min_difference_top_left = (0, 0)
+# row = 0
+# col = 1
+# while(row + template_height < img_height):
+#     while(col + template_width < img_width):
+#         pixel_difference = pixel_differences(img[row:, col:], template)
+#         if pixel_difference < min_pixel_difference:
+#             min_pixel_difference = pixel_difference
+#             min_difference_top_left = (col, row)
+#         col += 1
+#         print("process col")
+#     row += 1
+#
+#
+# img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGBA)
+# template= cv2.cvtColor(template, cv2.COLOR_BGRA2RGBA)
+#
+# plt.imshow(img)
+# plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+# plt.show()
 
 # All the 6 methods for comparison in a list
-# methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
-#             'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
-#
-# for meth in methods:
-#     img = img2.copy()
-#     method = eval(meth)
-#
-#     # Apply template Matching
-#     res = cv2.matchTemplate(img,template,method)
-#     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-#
-#     # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-#     if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
-#         top_left = min_loc
-#     else:
-#         top_left = max_loc
-#     bottom_right = (top_left[0] + w, top_left[1] + h)
-#
-#     cv2.rectangle(img,top_left, bottom_right, 255, 2)
-#
-#     plt.subplot(121),plt.imshow(res,cmap = 'gray')
-#     plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-#     plt.subplot(122),plt.imshow(img,cmap = 'gray')
-#     plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
-#     plt.suptitle(meth)
-#
-#     plt.show()
+methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
+            'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
+
+for meth in methods:
+    img = img2.copy()
+    method = eval(meth)
+
+    # Apply template Matching
+    res = cv2.matchTemplate(img,template,method)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+
+    # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+    if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
+        top_left = min_loc
+    else:
+        top_left = max_loc
+    bottom_right = (top_left[0] + template_width, top_left[1] + template_height)
+
+    cv2.rectangle(img,top_left, bottom_right, 255, 2)
+
+    plt.subplot(121),plt.imshow(res,cmap = 'gray')
+    plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122),plt.imshow(img,cmap = 'gray')
+    plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+    plt.suptitle(meth)
+
+    plt.show()
